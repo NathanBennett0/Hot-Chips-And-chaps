df388cc6cd6c2a3709164b0dd53dcffe467a2781
partenaom <naomiparte@gmail.com>
2022-09-12 13:36:49 +1200

    App init

---
+++ src/nz/ac/vuw/ecs/swen225/gp22/app/KeyStroke.java
@@ -0,0 +1,29 @@
+package nz.ac.vuw.ecs.swen225.gp22.app;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.HashMap;
+import java.util.Map;
+import javax.swing.SwingUtilities;
+
+public class KeyStroke implements KeyListener {
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
---
+++ src/nz/ac/vuw/ecs/swen225/gp22/app/Viewport.java
@@ -0,0 +1,16 @@
+package nz.ac.vuw.ecs.swen225.gp22.app;
+//import nz.ac.vuw.ecs.swen225.gp22.domain;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class Viewport extends JPanel {
+
+    //public Viewport(Model model){ this.model=model; }
+
+    public void paintComponent(Graphics g) {
+        //rendering stuff
+    }
+}
