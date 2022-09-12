6e258027c585053213f6b2145be02667f83b9d01
Christine Jayme <jaymechri@ecs.vuw.ac.nz>
2022-09-12 14:19:43 +1200

    Pseudo-code of what I need to get done in my role

---
+++ src/nz/ac/vuw/ecs/swen225/gp22/Recorder/Recorder.java
@@ -0,0 +1,16 @@
+package nz.ac.vuw.ecs.swen225.gp22.Recorder;
+
+public class Recorder {
+    //get (and parse) XML file from current game state (from Persistency Java package)
+    //store each game state until the end of the level (or is it until the end of the whole game?)
+    //store all of the game states for that game/level in an XML file
+    //Add functionality to the game's GUI to load a recorded game 
+    //Add the replay functionality to game's GUI.
+    //Also add these user-interface controls for the game's replay: step-by-step, auto-replay, set replay speed.
+
+    //Esentially store ALL states of the game so users can replay it. Add functionality so that users can choose 
+    //the way that they replay the game
+
+    
+
+}
