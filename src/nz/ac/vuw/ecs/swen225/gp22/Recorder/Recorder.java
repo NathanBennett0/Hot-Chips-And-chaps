016d134ee7e8ae96855f0d635ff4c4a080f1957a
Christine Jayme <jaymechri@ecs.vuw.ac.nz>
2022-09-12 14:24:52 +1200

    Update Recorder.java

--- src/nz/ac/vuw/ecs/swen225/gp22/Recorder/Recorder.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/Recorder/Recorder.java
@@ -5,8 +5,7 @@ public class Recorder {
     //store each game state until the end of the level (or is it until the end of the whole game?)
     //store all of the game states for that game/level in an XML file
     //Add functionality to the game's GUI to load a recorded game 
-    //Add the replay functionality to game's GUI.
-    //Also add these user-interface controls for the game's replay: step-by-step, auto-replay, set replay speed.
+    //Add the replay functionality to game's GUI: step-by-step, auto-replay, set replay speed.
 
     //Esentially store ALL states of the game so users can replay it. Add functionality so that users can choose 
     //the way that they replay the game
