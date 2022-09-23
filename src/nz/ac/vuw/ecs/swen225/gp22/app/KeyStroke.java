2bfacc86eba4fff25cdcfb20877660fcc6806621
partenaom <naomiparte@gmail.com>
2022-09-24 10:08:54 +1200

    Keys

--- src/nz/ac/vuw/ecs/swen225/gp22/app/KeyStroke.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/app/KeyStroke.java
@@ -4,26 +4,33 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
+
 import javax.swing.SwingUtilities;
 
+import nz.ac.vuw.ecs.swen225.gp22.domain.Chap;
+
 public class KeyStroke implements KeyListener {
-
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-
+	private Map<Integer,Runnable> onPressed = new HashMap<>();
+	//private Map<Integer,Runnable> onReleased = new HashMap<>();
+	
+	public void setAction(int keyCode, Runnable pressed) {
+		onPressed.put(keyCode, pressed);
+		//onReleased.put(keyCode, released);
 	}
+	
+	@Override
+	public void keyTyped(KeyEvent e) {}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
+		onPressed.getOrDefault(e.getKeyCode(),()->{}).run();
 
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-
+		//onReleased.getOrDefault(e.getKeyCode(),()->{}).run();
 	}
 
 }
