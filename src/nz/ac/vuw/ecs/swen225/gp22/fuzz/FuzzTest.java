1506b2adb138a9c09cb625f48cb0b0d60fb16675
Annie Cho <choanni@ecs.vuw.ac.nz>
2022-09-28 19:34:58 +1300

    Improved fuzz testing & new fuzz booleans in App

--- src/nz/ac/vuw/ecs/swen225/gp22/fuzz/FuzzTest.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/fuzz/FuzzTest.java
@@ -1,9 +1,12 @@
 package nz.ac.vuw.ecs.swen225.gp22.fuzz;
+import javax.swing.SwingUtilities;
+import java.awt.event.KeyEvent;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTimeout;
 import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 
+import java.awt.event.KeyEvent;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Random;
@@ -15,6 +18,7 @@ import org.junit.jupiter.api.Timeout;
 
 import nz.ac.vuw.ecs.swen225.gp22.app.App;
 import nz.ac.vuw.ecs.swen225.gp22.app.Game;
+import nz.ac.vuw.ecs.swen225.gp22.app.KeyStroke;
 import nz.ac.vuw.ecs.swen225.gp22.app.Main;
 
 /**
@@ -27,63 +31,69 @@ import nz.ac.vuw.ecs.swen225.gp22.app.Main;
 public class FuzzTest {
 
 	/**
-	 * Temporary testing for asserting time out To be removed once implemented fuzz
-	 * testing
-	 */
-	@Test
-	public void AssertTimeoutTest() {
-		ArrayList<Integer> myInts = new ArrayList<>();
-		for (int i = 0; i < 100000; i++) myInts.add(i);
-		assertTimeout(Duration.ofMillis(1), () -> {
-			for (int i = 0; i < myInts.size(); i++) {
-				System.out.println(myInts.get(i));
-				// Must execute this code in the time frame provided
-			}
-		});
-	}
-	
-	/**
 	 * Test for level one
 	 */
 	@Test
 	public void FuzzTest1() {
 		
-		assertTimeout(Duration.ofSeconds(10), () -> {
-			long timer = System.currentTimeMillis() + 10 * 1000; // Timer to stop while loop
-			Main.main(null); // Start the main before the testing loop
+		System.out.println("FuzzTest 1 is called.");
+		//Main.main(null);
+		App app = new App();
+		
+		while(App.initializeDone == false) System.out.print(""); // While the main menu is booting, wait
+		System.out.println("Initializing of window done. Timed fuzz test begins now.");
+		
+		assertTimeout(Duration.ofSeconds(8), () -> {
+			// Timer to stop while loop
+			long timer = System.currentTimeMillis() + 8 * 1000; 
 			
-			boolean started = false;
-
 			while(true) {
-			    while(started) {	
-			    	
-			    	getRandomDirection();
+				// Constantly try to start the code below
+				System.out.print("");
+			    while(App.fuzzStarted) {
+			    	// Begins once the start button is pressed
+			    	int direction = getRandomDirection();
+			    	//KeyStroke.keyPressed(direction);
 			        if(System.currentTimeMillis() > timer) break; // Breaks test loop
 			    }
-			    if(System.currentTimeMillis() > timer) break; // Breaks game loop
+			    if(System.currentTimeMillis() > timer) {
+			    	App.fuzzStarted = false;
+			    	App.initializeDone = false;
+			    	System.out.println("fuzzStarted = " + App.fuzzStarted + "    initializeDone = " + App.initializeDone);
+			    	break; // Breaks game loop
+			    }
 			}
 		});
 	}
 	
+
+
+	
 	
 	/**
 	 * Method that gets a random direction for Chap to move
-	 * @return int 1-4, each representing a direction
+	 * @return Keystroke constant, each representing a direction
 	 */
 	public int getRandomDirection() {
 		Random r = new Random();
-		int random = 1 + r.nextInt(4);
+		int random = 1 + r.nextInt(4); // Generates a random number 1-4
 		switch(random) {
 			case 1:
 		    	System.out.println("Fuzz.java: random direction is UP.");
+		    	return KeyEvent.VK_W; // Keystroke constant that represents W
 		    case 2:
 		    	System.out.println("Fuzz.java: random direction is DOWN.");
+		    	return KeyEvent.VK_S; // Keystroke constant that represents S
 		    case 3:
 		    	System.out.println("Fuzz.java: random direction is LEFT.");
+		    	return KeyEvent.VK_A; // Keystroke constant that represents A
 		    case 4:
 		    	System.out.println("Fuzz.java: random direction is RIGHT.");
+		    	return KeyEvent.VK_D; // Keystroke constant that represents D
+		    default:
+		    	System.out.println("Random number failed");
+		    	return 0; // Should never reach this point
 		}
-		return random;
 	}
 		
 
