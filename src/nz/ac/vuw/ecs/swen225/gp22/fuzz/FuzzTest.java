37a8795a01b66ce0a1f702594a6d3ff1aeb34580
Annie Cho <choanni@ecs.vuw.ac.nz>
2022-09-28 15:48:21 +1300

    Created getRandomDirection, begins game

--- src/nz/ac/vuw/ecs/swen225/gp22/fuzz/FuzzTest.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/fuzz/FuzzTest.java
@@ -8,10 +8,14 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Random;
 
+import javax.swing.SwingUtilities;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 
+import nz.ac.vuw.ecs.swen225.gp22.app.App;
 import nz.ac.vuw.ecs.swen225.gp22.app.Game;
+import nz.ac.vuw.ecs.swen225.gp22.app.Main;
 
 /**
  * Class that runs the two fuzz tests for the Chips&Chaps game. Generates random
@@ -29,9 +33,7 @@ public class FuzzTest {
 	@Test
 	public void AssertTimeoutTest() {
 		ArrayList<Integer> myInts = new ArrayList<>();
-		for (int i = 0; i < 100000; i++) {
-			myInts.add(i);
-		}
+		for (int i = 0; i < 100000; i++) myInts.add(i);
 		assertTimeout(Duration.ofMillis(1), () -> {
 			for (int i = 0; i < myInts.size(); i++) {
 				System.out.println(myInts.get(i));
@@ -46,59 +48,50 @@ public class FuzzTest {
 	@Test
 	public void FuzzTest1() {
 		
-		assertTimeout(Duration.ofSeconds(5), () -> {
+		assertTimeout(Duration.ofSeconds(10), () -> {
+			long timer = System.currentTimeMillis() + 10 * 1000; // Timer to stop while loop
+			Main.main(null); // Start the main before the testing loop
 			
-			// Creates timer to stop while loop
-			// Assert timeout will always wait for loop to execute
-			long start = System.currentTimeMillis();
-			long finish = start + 5 * 1000; 
-			
-			
-			// Declare variables before the testing loop
-			Random r = new Random();
-			int random = 1 + r.nextInt(6);
-			
-			// Begin testing loop
-		    while(true) {
-		    	System.out.println(random); 
-		        if(System.currentTimeMillis() > finish) {
-		        	break; // Check if time has run out
-		        }
-		    }
+			boolean started = false;
+
+			while(true) {
+			    while(started) {	
+			    	
+			    	getRandomDirection();
+			        if(System.currentTimeMillis() > timer) break; // Breaks test loop
+			    }
+			    if(System.currentTimeMillis() > timer) break; // Breaks game loop
+			}
 		});
 	}
-			
-
-	// Idea for fuzz testing
-	//
-	// Load a game level
-	// Array of all of the possible moves
-	// (Up, down, left, right) Keys and doors are used automatically
-	// Randomly generate 1-4 to pick what way to move
-	// Store in an Arraylist with count of which moves have been done
-	// Randomly call a method inside the app module
-	//
-	// If chap is in current position
-	// Store in a hashmap of (Position, Direction)
-	// If one of the directions haven't been explored yet, then preferably pick that
-	//
-	// If error or exception is detected, then log to git
-	// Time out
-
-	// Assertions to make
-	// If the next block is an entity, check that move was applied properly
-	// e.g. did not move if the next block is wall, did not move if next block is a
-	// locked door
-	// did move if the door was locked (check that door is no longer present)
-	// if the next block is a key, check that key is no longer present
+	
+	
+	/**
+	 * Method that gets a random direction for Chap to move
+	 * @return int 1-4, each representing a direction
+	 */
+	public int getRandomDirection() {
+		Random r = new Random();
+		int random = 1 + r.nextInt(4);
+		switch(random) {
+			case 1:
+		    	System.out.println("Fuzz.java: random direction is UP.");
+		    case 2:
+		    	System.out.println("Fuzz.java: random direction is DOWN.");
+		    case 3:
+		    	System.out.println("Fuzz.java: random direction is LEFT.");
+		    case 4:
+		    	System.out.println("Fuzz.java: random direction is RIGHT.");
+		}
+		return random;
+	}
+		
 
 
 	/**
 	 * Test for level two
 	 */
 	@Test
-	public static void FuzzTest2() {
-
-	}
+	public static void FuzzTest2() {}
 
 }
