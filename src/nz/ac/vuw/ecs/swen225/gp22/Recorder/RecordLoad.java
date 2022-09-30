0517d3428ef8ba981ad6e2c1f95ce6eb7425d10c
Christine Jayme <jaymechri@ecs.vuw.ac.nz>
2022-09-30 13:01:26 +1300

    Update RecordLoad.java

--- src/nz/ac/vuw/ecs/swen225/gp22/Recorder/RecordLoad.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/Recorder/RecordLoad.java
@@ -1,78 +1,159 @@
 package nz.ac.vuw.ecs.swen225.gp22.Recorder;
-
-import javax.swing.*;
 import java.io.*;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.SortedMap;
+import java.io.InputStream;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.OutputStream;
+
+//create map for move and time at the same time
+//create your own timer in the App so that each time a move is done, you can add that move and time into the map
+//Once the whole level is finished and you have that array, you can then load the level
+
+/**
+ * TO DO: Find how to store the levels as well.
+ */
 
 public class RecordLoad {
-    private ArrayList<Move> moves;
 
-    public RecordSaver(ArrayList<Move> moves) {
-        this.moves = moves;
-        save(null);
+    // private final double time;
+    private final static LinkedHashMap<Double, String> recordMap = new LinkedHashMap<Double, String>(); // LinkedHashMap
+                                                                                                        // that stores
+                                                                                                        // the time and
+                                                                                                        // move
+    private static double count = 0; // counts level within program
+
+    /**
+     * Constructor for loadRecord
+     * 
+     * @param time that the move was made
+     * @param move the type of move done
+     */
+    public RecordLoad(Double time, String move) throws TransformerException, ParserConfigurationException, IOException {
+        recordMap.put(time, move);
+
+        // for testing
+    
+        if (recordMap.containsKey(0.0)) {
+            loadDocRecord();
+        }
+        // find out when button is clicked then...
+        // if(endGame = endofGame){
+        // loadDocRecord();
+        // }
     }
 
-    public void save(String fileName){
-        //get name of new file if not provided
-        if (fileName == null || fileName.isBlank() || fileName.isEmpty()){
-            fileName = getFileName("Please enter a name for the recording file (without extension):");
-            if (fileName == null) return; //cancel
+    public static void main(String[] agrs) {
+        try{
+        RecordLoad l = new RecordLoad(0.007, "up");
+        RecordLoad x = new RecordLoad(0.001, "down");
+        RecordLoad n = new RecordLoad(0.008, "down");
+        RecordLoad a = new RecordLoad(0.0, "left");
+    }catch(TransformerException , ParserConfigurationException, IOException exception){
+
+    }
+
+    }
+
+    // if constructor shouldn't be used:
+
+    /**
+     * public loadRecord(){
+     * 
+     * }
+     * 
+     * public void setMap(Double time, String move){
+     * sortedMap.put(time,move);
+     * }
+     * @throws IOException
+     */
+    // public void loadDocRecord() throws ParserConfigurationException,
+    // TransformerException {
+    public void loadDocRecord() throws TransformerException, ParserConfigurationException, IOException {
+        // loads the xml file specified then creates a document object
+
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+        // sets root element as the level
+        if (count > 1) {
+            Document doc = docBuilder.newDocument();
+            Element rootElement = doc.createElement("Level Two");
+            doc.appendChild(rootElement);
         }
-        fileName += ".json";
-        StringBuilder jsonRecording = new StringBuilder();
+        Document doc = docBuilder.newDocument();
+        Element rootElement = doc.createElement("Level One");
+        doc.appendChild(rootElement);
 
-        //make text to store in json format
-        jsonRecording.append("{\n\t\"Actions\": [\n");
-        for (int i = 0; i < moves.size(); i++) {
-            jsonRecording.append("\t\t{\n");
+        Element recElems = doc.createElement("record elements"); // the overall element
+        rootElement.appendChild(recElems); // appends it into the new document
 
-            //player or bug
-            jsonRecording.append("\t\t\t\"").append(moves.get(i).getMover()).append("\": \"").append(moves.get(i)).append("\"\n");
+        // below code inspired by
+        // https://www.tutorialspoint.com/java_xml/java_dom_create_document.htm
+        // essentially iterates over moves and times (the key and value) in the map and
+        // appends it to the new element
+        for (Map.Entry<Double, String> entry : recordMap.entrySet()) {
 
-            jsonRecording.append(i < moves.size()-1 ? "\t\t},\n":"\t\t}\n");
+            Element recordElements = doc.createElement("Move + Time"); // header for all moves and time in the doc
+
+            Attr attrMove = doc.createAttribute("move"); // create new attribute for the move
+
+            attrMove.setValue(entry.getValue()); // set the move of the attribute
+
+            recordElements.setAttributeNode(attrMove); // add the attribute to the element
+
+            Attr attrTime = doc.createAttribute("time"); // create new attribute for that time
+
+            attrTime.setValue(entry.getKey() + ""); // set the time for that attribute
+
+            recordElements.setAttributeNode(attrTime); // set the new attribute in the element
+
+            recElems.appendChild(recordElements); // add element to the overall "moves" header
         }
-        jsonRecording.append("\t]\n}");
 
-        //save to file
+        // if(gameState = endOfGame){
 
-        File savingFile = new File("Recordings/"+fileName);
-        if (savingFile.exists()){
-            //check if file exists and ask user if they want
-            //to override existing file based on https://stackoverflow.com/questions/1816673/how-do-i-check-if-a-file-exists-in-java
-            int wantTo = JOptionPane.showConfirmDialog(null, "Are you sure you want to override "+fileName+"?");
-            if (wantTo != JOptionPane.YES_OPTION){
-                //try again
-                save("Please enter a name for the recording file (without extension):");
-                return;
-            }
-        }
-        try {
-            //write contents to file or make new file
-            // - based on https://stackoverflow.com/questions/52581404/java-create-a-new-file-or-override-the-existing-file
-            BufferedWriter newFile = new BufferedWriter(new FileWriter(savingFile));
-            newFile.write(jsonRecording.toString());
-            newFile.close();
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
+        // write dom document to a file
+        try (FileOutputStream output = new FileOutputStream("C::\\gameRecord.xml")) {
+            writeXml(doc, output);
+        } catch (TransformerException e) {
+            System.out.println("File load failed");
             e.printStackTrace();
         }
-
-        JOptionPane.showMessageDialog(null, "File saved to Recordings folder.");
+        // }
     }
 
-    public static String getFileName(String message){
-        String fileName = "";
-        while (fileName.isEmpty() || fileName.isBlank()){
-            try {
-                fileName = JOptionPane.showInputDialog(message); //todo error checking
-            } catch (NullPointerException ignored){
-                JOptionPane.showMessageDialog(null, "File save cancelled.");
-                return null;
-            }
-
-            message = "Filename is empty. Please enter a valid filename.";
+    /**
+     * Writes document to the output stream
+     * 
+     * @param d      the document created in loadRecord of the moves and its
+     *               corresponding times
+     * @param output the file OutPutStream
+     * @throws TransformerException
+     */
+    private static void writeXml(Document d, OutputStream output) throws TransformerException {
+        try {
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(d);
+            StreamResult result = new StreamResult(output);
+            transformer.transform(source, result);
+        } catch (TransformerException e) {
+            System.out.println("File load failed");
+            e.printStackTrace();
         }
-
-        return fileName;
     }
 }
