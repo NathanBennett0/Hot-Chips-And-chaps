83c2cae5dfc6ffb91d8d1d448954235eded00a79
wipateella <wipateella@ecs.vuw.ac.nz>
2022-10-03 13:53:10 +1300

    Fixing the camera so that the chap is always focused

--- src/nz/ac/vuw/ecs/swen225/gp22/renderer/GamePanel.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/renderer/GamePanel.java
@@ -9,6 +9,7 @@ import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 
 import javax.swing.ImageIcon;
@@ -16,37 +17,37 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
+<<<<<<< HEAD
 import nz.ac.vuw.ecs.swen225.gp22.app.App;
 import nz.ac.vuw.ecs.swen225.gp22.app.Game;
+=======
+import nz.ac.vuw.ecs.swen225.gp22.app.*;
+>>>>>>> 80c6be8 (Fixing the camera so that the chap is always focused)
 import nz.ac.vuw.ecs.swen225.gp22.domain.*;
 
 public class GamePanel extends JPanel implements ActionListener{
 	final int IMAGE_DIM = 62;
 	final int BOARD_DIM = 558;
 	
-	Tile[][] board;
-	Tile[][] test = new Tile[9][9]; // This 2D array is for testing purposes only
+	Tile[][] board = new Tile[9][9];
 	Timer timer; 
+	Maze m;
+	App app; 
 	
 
-	public GamePanel(Maze m, App a) {		
-		// This is just to fill the array for testing
+	public GamePanel(Maze m, App a) {	
+		this.m = m;
+		this.app = a; 
     	for(int x = 0; x < 9; x++) {
     		for(int y = 0; y < 9; y++) {
-    			//test[x][y] = new Free(new Location(x,y));
-				test[x][y] = m.grid[x+3][y+3];
+				board[x][y] = m.grid[x][y];
     		}
     	}
     	
-    	// For testing purposes
-//    	test[3][3] = new Wall(new Location(3,3));
-//    	test[7][6] = new Treasure(new Location(3,3), false);
-//    	test[4][4] = new Chap(new Location(3,3), null);
-    	
     	this.setBounds(62,35,BOARD_DIM,BOARD_DIM);
     	this.setLayout(new GridLayout(9,9));
-//    	timer = new Timer(100, this); // Timer works in milliseconds
-//		timer.start();
+    	timer = new Timer(30, this); // Timer works in milliseconds
+		timer.start();
 	}
 	
 	/**
@@ -56,7 +57,7 @@ public class GamePanel extends JPanel implements ActionListener{
 		Graphics2D g2d = (Graphics2D) g;
 		for(int x = 0; x < 9; x++) {
     		for(int y = 0; y < 9; y++) {
-    			Image i = test[x][y].getImg().image.getScaledInstance(IMAGE_DIM,IMAGE_DIM,Image.SCALE_SMOOTH);
+    			Image i = board[x][y].getImg().image.getScaledInstance(IMAGE_DIM,IMAGE_DIM,Image.SCALE_SMOOTH);
     			g2d.drawImage(i, x * IMAGE_DIM, y * IMAGE_DIM, null);
     		}
     	}
@@ -66,21 +67,46 @@ public class GamePanel extends JPanel implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// get the board
-		
 		/**
-		for(Tile[] t: test) {
-			for(Tile tile: t) {
-				if(tile instanceof Chap) {
-					
-					
+		board = app.getGame().phase().maze().getGrid();
+		
+		Controller c = app.getGame().phase().controller();
+		c.keyPressed(KeyEvent.VK_A);
+		
+		for(int x = 0; x < board.length; x++) {
+    		for(int y = 0; y < board.length; y++) {
+				if(board[x][y] instanceof Chap) {
+					System.out.println(x);
+					System.out.println(y);
+					System.out.println("");
+					System.out.println(board[x][y].getLocation().getX());
+					System.out.println(board[x][y].getLocation().getY());
 				}
+    		}
+    	}
+		
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		
+		
+		
+		*/
+		// ----------------
+		//Controller c = app.getGame().phase().controller();
+		//c.keyPressed(KeyEvent.VK_A);
+		
+		Tile[][] temp = app.getGame().phase().maze().getGrid();
+		for(int x = 0; x < temp.length; x++) {
+    		for(int y = 0; y < temp.length; y++) {
+    			if(temp[x][y] instanceof Chap) {
+    				updateCamera(x,y,temp);
+    			}
+    			
 			}
-			
 		}
-		*/ 
 		
 		repaint(); 
-		
 		
 	}
 	
@@ -91,6 +117,18 @@ public class GamePanel extends JPanel implements ActionListener{
 	 */
 	 public void updateBoard(Tile[][] b) {
 		 this.board = b; 
+	 }
+	 
+	 
+	 public void updateCamera(int x, int y, Tile[][] temp){
+		 int shiftX = x - 4; 
+		 int shiftY = y - 4;
+		 
+		 for(int i = 0; i < 9; i++) {
+	    	for(int j = 0; j < 9; j++) {
+	    		board[i][j] = temp[i + shiftX][j+ shiftY];
+    		}
+    	}
 	 }
 
 	 
@@ -106,7 +144,7 @@ public class GamePanel extends JPanel implements ActionListener{
 	    	for(int x = 0; x < 9; x++) {
 	    		for(int y = 0; y < 9; y++) {
 	    			 var tileLabel = new JLabel();
-	    			 ImageIcon icon = new ImageIcon(test[x][y].icon.image.getScaledInstance(IMAGE_DIM,IMAGE_DIM,Image.SCALE_SMOOTH));
+	    			 ImageIcon icon = new ImageIcon(board[x][y].icon.image.getScaledInstance(IMAGE_DIM,IMAGE_DIM,Image.SCALE_SMOOTH));
 	    			 tileLabel.setIcon(icon);
 	    			 this.add(tileLabel);
 	    		}
