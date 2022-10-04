3b1de80e259a93383515ecbe0a403175458060ac
partenaom <naomiparte@gmail.com>
2022-10-05 10:28:59 +1300

    Key listener fixed

--- src/nz/ac/vuw/ecs/swen225/gp22/app/App.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/app/App.java
@@ -9,9 +9,11 @@ import java.awt.event.WindowEvent;
 import java.text.SimpleDateFormat;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.event.KeyListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -33,14 +35,33 @@ import java.awt.Color;
 
 public class App extends JFrame {
 	
-    //Initializing Variables
+    //Initializing Constants
 	public final static int WIDTH = 900;
 	public final static int HEIGHT = 680;
+    public final static int TIMELIMIT_ONE = 90000;
+    public final static int TIMELIMIT_TWO = 180000;
+
+    //Game variables
 	Game game;
+    Timer timer;
+    JPanel currentPanel;
+    int timeLeft;  //timeleft
+
     Runnable newPanel = ()->{};
-    
     private boolean stopTimer = true;
-    
+    private boolean pauseTimer = false;
+
+    //Menu Items
+    JMenuBar mb=new JMenuBar();
+    JMenuItem home=new JMenuItem("Home");
+    JMenu start=new JMenu("Go to");
+    JMenuItem lvl1=new JMenuItem("Level 1");
+    JMenuItem lvl2=new JMenuItem("Level 2");
+    JMenuItem load=new JMenuItem("Load Game");
+    JMenuItem pause=new JMenuItem("Pause");
+    JMenuItem save=new JMenuItem("Save");  
+    JMenuItem exit=new JMenuItem("Exit");  
+
     //Boolean variables for fuzz testing
     private boolean fuzzStarted = false;
     private boolean initializeDone = false;
@@ -77,6 +98,8 @@ public class App extends JFrame {
     
     public void mainMenu(){
     	System.out.println("App.java: mainMenu() called.");
+
+
         newPanel.run();
         restart.run();
         var p = new JPanel();
@@ -97,6 +120,8 @@ public class App extends JFrame {
         p.add(play);
         p.add(sp);
         
+        currentPanel = p;
+        changeKeyListener(new Controller(this));
         newPanel = ()->{ remove(p);};
         pack();
     }
@@ -117,22 +142,13 @@ public class App extends JFrame {
 
         getContentPane().add(BorderLayout.CENTER, p);
         newPanel = ()->{ remove(p);};
+        currentPanel = p;
 
         pack();
     }
     
     public void menuBar() {
     	System.out.println("App.java: menuBar() called.");
-    	//DECLARE
-    	JMenuBar mb=new JMenuBar();
-    	JMenuItem home=new JMenuItem("Home");
-    	JMenu start=new JMenu("Go to");
-    	JMenuItem lvl1=new JMenuItem("Level 1");
-    	JMenuItem lvl2=new JMenuItem("Level 2");
-    	JMenuItem load=new JMenuItem("Load Game");
-    	JMenuItem pause=new JMenuItem("Pause");
-    	JMenuItem save=new JMenuItem("Save");  
-    	JMenuItem exit=new JMenuItem("Exit");  
     	
     	home.setPreferredSize(new Dimension(180, home.getPreferredSize().height));
     	start.setPreferredSize(new Dimension(180, start.getPreferredSize().height));
@@ -143,7 +159,7 @@ public class App extends JFrame {
     	
     	//FUNCTIONS
     	home.addActionListener((e)->mainMenu()); //connect to the main menu pane - pop up window asking what to do
-    	pause.addActionListener((e)->System.out.println("Pause")); //pop up window and pause timer and disable every action (change focus)
+    	pause.addActionListener((e)->pauseTimer = !pauseTimer); //pop up window and pause timer and disable every action (change focus)
         lvl1.addActionListener((e)->levelOne());
         lvl2.addActionListener((e)->levelTwo());
         exit.addActionListener((e)->System.exit(0));
@@ -159,46 +175,50 @@ public class App extends JFrame {
     	mb.add(exit);
     	this.setJMenuBar(mb);
     }
+
     
     /*---- GAME MECHANICS ---------------------------------------------------*/
     
-    void setPhase(Phase phase){
+    void setPhase(Phase phase, int time){
     	System.out.println("App.java: setPhase() called.");
         newPanel.run();
         stopTimer = false;
         game = new Game(phase, this);
-        game.addKeyListener(phase.controller());
+        
         game.setFocusable(true);
+        timeLeft = time;
        
         ActionListener countDown = new ActionListener(){
-        	int timeLeft = phase.level()==2?180000:90000; //level 2: 3 mins, level 1: 1.5 mins
-        	
 		    public void actionPerformed(ActionEvent e){
-		        timeLeft -= 250;
-		        SimpleDateFormat df=new SimpleDateFormat("mm:ss");
+                SimpleDateFormat df=new SimpleDateFormat("mm:ss");
+
+                if(!pauseTimer) timeLeft -= 250;
+
 		        game.tLeft.setText(df.format(timeLeft));
 		        game.repaint();
+
 		        if(timeLeft<=0 || stopTimer){
 		        	((Timer)e.getSource()).stop();
 		        }
 		    }
 		};
 		
-		Timer timer=new Timer(250, countDown);
+		timer=new Timer(250, countDown);
         getContentPane().add(BorderLayout.CENTER, game);
-                
-        newPanel = ()->{ remove(game);};
+        
+        currentPanel = game;
+        changeKeyListener(phase.controller());
+        
+        newPanel = ()->{ remove(game); timer.stop();};
 
         pack();
+        currentPanel.requestFocus();
         timer.start();
         fuzzStarted = true; // For fuzz testing
     }
-    
+
     /**
      * Loads the level 1 of the game and creates the Maze accordingly
-     * 
-     * @param levelname
-     * @param num - level
      */
     public void levelOne() {
         System.out.println("loading lvl 1");
@@ -206,14 +226,12 @@ public class App extends JFrame {
         Maze m = new Maze(lvl);
         lvl.getChap().setMaze(m); 
         // now have the maze object
-        setPhase(new Phase(m, new Controller(this, lvl.getChap()), 1));
+        setPhase(new Phase(m, new Controller(this, lvl.getChap()), 1), TIMELIMIT_ONE);
     }
     
     /**
      * Loads the level 1 of the game and creates the Maze accordingly
      * 
-     * @param levelname
-     * @param num - level
      */
     public void levelTwo() {
         System.out.println("loading lvl 2");
@@ -221,7 +239,7 @@ public class App extends JFrame {
         Maze m = new Maze(lvl);
         lvl.getChap().setMaze(m); 
         // now have the maze object
-        setPhase(new Phase(m, new Controller(this, lvl.getChap()), 2));
+        setPhase(new Phase(m, new Controller(this, lvl.getChap()), 2), TIMELIMIT_TWO);
     }
 
     /**
@@ -238,6 +256,13 @@ public class App extends JFrame {
     	return this.game;
     }
     
+    public void changeKeyListener(KeyListener keyListener) {
+        if (currentPanel.getKeyListeners().length > 0) {
+            currentPanel.removeKeyListener(currentPanel.getKeyListeners()[0]);
+        }
+        currentPanel.addKeyListener(keyListener);
+        currentPanel.setFocusable(true);
+    }
     
     
     /*--- FOR TESTING -------------------------------------------------------*/
