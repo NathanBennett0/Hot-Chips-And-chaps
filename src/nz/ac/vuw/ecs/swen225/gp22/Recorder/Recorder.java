d4e57ce2767cc3f08f5e1fc81a1e73c31090be8a
Christine Jayme <jaymechri@ecs.vuw.ac.nz>
2022-10-12 03:27:41 +1300

    close #48 Created method that can save a move into an XML format in Recorder.java

--- src/nz/ac/vuw/ecs/swen225/gp22/Recorder/Recorder.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/Recorder/Recorder.java
@@ -1,17 +1,44 @@
 package nz.ac.vuw.ecs.swen225.gp22.Recorder;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.XMLWriter;
+
 public class Recorder {
-	// get (and parse) XML file from current game state (from Persistency Java
-	// package)
-	// store each game state until the end of the level (or is it until the end of
-	// the whole game?)
-	// store all of the game states for that game/level in an XML file
-	// Add functionality to the game's GUI to load a recorded game
-	// Add the replay functionality to game's GUI: step-by-step, auto-replay, set
-	// replay speed.
+	private final List<Move> movesList;
+	private final int level;
+	
+	/**
+	 * Constructing a new recorder
+	 */
+	public Recorder(int L){
+		level = L;
+		movesList = new ArrayList<>();
+	}
 
-	// Esentially store ALL states of the game. Using that data, add functionality
-	// in GUI so that users can choose
-	// the way that they replay the game
+	/**
+	 * Save a move to an XML file format
+	 */
+	public void saveMove(){
+		Document doc = DocumentHelper.createDocument();
+		Element root = doc.addElement("Game").addAttribute("level", String.valueOf(level));
+		movesList.forEach(move -> root.add(move.saveXML()));
 
+		String fName = System.getProperty("user.dir") + "/recorder/files" + LocalDateTime.now().toString() + ".chipvschap.xml";
+		try(FileWriter write = new FileWriter(fName, StandardCharsets.UTF_8)){
+			(new XMLWriter(write, OutputFormat.createPrettyPrint())).write(doc);
+		}catch(IOException e){
+			e.printStackTrace();
+			System.out.println("saving file not successful");
+		}
+	}
 }
