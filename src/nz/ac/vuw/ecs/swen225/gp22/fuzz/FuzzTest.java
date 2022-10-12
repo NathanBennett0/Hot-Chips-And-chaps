6a6cc7abda9093ba064488ce9f7fa1167c291868
Annie Cho <choanni@ecs.vuw.ac.nz>
2022-10-13 00:08:01 +1300

    Removed domain dependencies, implemented test 2, split into methods

--- src/nz/ac/vuw/ecs/swen225/gp22/fuzz/FuzzTest.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/fuzz/FuzzTest.java
@@ -21,10 +21,6 @@ import nz.ac.vuw.ecs.swen225.gp22.app.Game;
 import nz.ac.vuw.ecs.swen225.gp22.app.KeyStroke;
 import nz.ac.vuw.ecs.swen225.gp22.app.Main;
 import nz.ac.vuw.ecs.swen225.gp22.app.Controller;
-import nz.ac.vuw.ecs.swen225.gp22.domain.Chap;
-import nz.ac.vuw.ecs.swen225.gp22.domain.Maze;
-import nz.ac.vuw.ecs.swen225.gp22.domain.Tile;
-import nz.ac.vuw.ecs.swen225.gp22.domain.Wall;
 
 /**
  * Class that runs the two fuzz tests for the Chips&Chaps game. 
@@ -51,53 +47,15 @@ public class FuzzTest {
 		
 		System.out.println("FuzzTest 1 is called.");
 		makeStrategyBoard();
-		App app = new App();
+		App app = App.getInstance();
 		
 		while(!app.initializeDone()) System.out.print(""); // While the main menu is booting, wait
 		System.out.println("Initializing of window done. Timed fuzz test begins now.");
 		
-		
 		long timer = System.currentTimeMillis() + 60 * 1000; // Timer to stop while loop
-		app.levelOne(); // This will change fuzzStarted to true and load L1
+		app.phaseOne(); // This will change fuzzStarted to true and load L1
 		assert app.fuzzStarted() == true;
-		
-
-		while(app.fuzzStarted()) {
-			
-			pause(app,95);
-			Controller c = app.getGame().phase().controller();
-			Maze m = app.getGame().phase().maze();
-			assert c != null && m != null;
-			
-			
-			// Use intelligence to find the next direction
-			int direction = pickDirection();
-			c.keyPressed(direction);
-			
-			
-			// Check the location of the chap
-			updateLocation(m);
-			System.out.println("Prev Location: " + prevX + "," + prevY);
-			System.out.println("Curr Location: " + currentX + "," + currentY);
-			
-			
-			
-			// Use intelligence so the wall does not get tried again
-			if(prevX == currentX && prevY == currentY) {
-				int wallX = findWallX(prevDirection); // Find x of the next tile over (where we wanted to go)
-				int wallY = findWallY(prevDirection); // Find y of the next tile over (where we wanted to go)
-				
-				if(m.getGrid()[wallX][wallY] instanceof Wall) {
-					System.out.println("WE JUST HIT A WALL at " + wallX + "," + wallY);
-					HashMap<String, Integer> position = strategy[currentX][currentY];
-					position.put(prevDirection, 1000); // So we do not try it again
-				}
-			}
-			
-			
-			// Checks if the timer has exceeded
-			if(checkTimer(timer)) break;
-		}
+		intelligence(app, timer);
 
 	}
 	
@@ -106,7 +64,20 @@ public class FuzzTest {
 	 * Test for level two
 	 */
 	@Test
-	public void FuzzTest2() { }
+	public void FuzzTest2() { 
+	    System.out.println("FuzzTest 2 is called.");
+        makeStrategyBoard();
+        App app = App.getInstance();
+        
+        while(!app.initializeDone()) System.out.print("");
+        System.out.println("Initializing of window done. Timed fuzz test begins now.");
+        
+        long timer = System.currentTimeMillis() + 60 * 1000; 
+        app.phaseTwo(); // This will change fuzzStarted to true and load L3
+        assert app.fuzzStarted() == true;
+        intelligence(app, timer);
+	    
+	}
 	
 	
 	/*----- HELPER METHODS ------------------------------------------------------------- */
@@ -135,12 +106,46 @@ public class FuzzTest {
 	
 	
 	/**
-	 * Locates the chap on the maze and updates the current position
-	 * @param m Maze object found from App.java
+	 * Intelligence used to run the tests
+	 * @param app
+	 * @param timer
 	 */
-	public void updateLocation(Maze m) {
-		currentX = m.player.l.getX();
-		currentY = m.player.l.getY();
+	public void intelligence(App app, long timer) {
+	    while(app.fuzzStarted()) {
+            
+            pause(app,95);
+            Controller c = app.getGame().phase().controller();
+            assert c != null;
+            
+            
+            // Use intelligence to find the next direction
+            int direction = pickDirection();
+            c.keyPressed(direction);
+            
+            
+            // Check the location of the chap
+            currentX = app.updateLocationX();
+            currentY = app.updateLocationY();
+            System.out.println("Prev Location: " + prevX + "," + prevY);
+            System.out.println("Curr Location: " + currentX + "," + currentY);
+            
+            
+            
+            // Use intelligence so the wall does not get tried again
+            if(prevX == currentX && prevY == currentY) {
+                int wallX = findWallX(prevDirection); // Find x of the next tile over (where we wanted to go)
+                int wallY = findWallY(prevDirection); // Find y of the next tile over (where we wanted to go)
+                
+                if(app.isWall(wallX, wallY)) {
+                    System.out.println("WE JUST HIT A WALL at " + wallX + "," + wallY);
+                    HashMap<String, Integer> position = strategy[currentX][currentY];
+                    position.put(prevDirection, 1000); // So we do not try it again
+                }
+            }
+            
+            // Checks if the timer has exceeded
+            if(checkTimer(timer)) break;
+        }
 	}
 	
 	
@@ -192,10 +197,9 @@ public class FuzzTest {
 	
 	
 	/**
-	* Beginning intelligence
-	* THINGS I WANT TO DO
-	* IF THEY HAVE ALL BEEN EXPLORED, GO WhERE least explored!!!
-	* DO NOT MOVE BACK INTO OLD PLACE!!!!!
+	* Picks a direction for the chap to travel in
+	* Chap cannot move backwards
+	* Chap also moves in the direction least explored
 	*/
 	public int pickDirection() {
 		
@@ -215,16 +219,11 @@ public class FuzzTest {
 	    String dirExplored = direction;
 	    
 	    for(int i = 0; i < directions.size(); i++) {
-	    	System.out.println("Direction: " + directions.get(i) + "   Times: " + currentPos.get(directions.get(i)));
 	    	if(currentPos.get(directions.get(i)) <= numExplored) {
 	    		numExplored = currentPos.get(directions.get(i));
 	    		dirExplored = directions.get(i);
 	    	}
 	    }
-	    
-	    System.out.println("Picked: " + dirExplored);
-	    System.out.println();
-	    System.out.println();
 	    
 	    
 	    // Bump up that direction's exploration and go that direction
@@ -237,6 +236,7 @@ public class FuzzTest {
 	}
 
 		
+	
 	/**
 	 * Synchronizes the threads so the game can wait if when called
 	 * @param a App instance running the game thread
