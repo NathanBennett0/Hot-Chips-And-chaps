ead6702433d932eae5634c733849498ccbbc3fd0
Christine Jayme <jaymechri@ecs.vuw.ac.nz>
2022-10-12 19:40:07 +1300

    Turning directionMove.java into a class. Allows two constructors

--- src/nz/ac/vuw/ecs/swen225/gp22/Recorder/directionMove.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/Recorder/directionMove.java
@@ -1,11 +1,25 @@
+
 package nz.ac.vuw.ecs.swen225.gp22.Recorder;
+
 import nz.ac.vuw.ecs.swen225.gp22.app.App;
 import nz.ac.vuw.ecs.swen225.gp22.app.Controller;
 import org.dom4j.Element;
 import org.dom4j.tree.BaseElement;
 
-public record directionMove(App app, int keyCode) implements Move{
-    //public record directionMove(App app, String dir, int keyCode) implements Move{
+public class directionMove implements Move {
+    public int keyCode = 0;
+    App app;
+
+    public directionMove(App a, int code) {
+        keyCode = code;
+        app = a;
+    }
+
+    public directionMove(int code) {
+        keyCode = code;
+        // use extra constructor for RecordLoad
+
+    }
 
     /**
      * Saves move as an XML element
@@ -14,7 +28,8 @@ public record directionMove(App app, int keyCode) implements Move{
      */
     @Override
     public Element saveXML() {
-        return new BaseElement("move").addAttribute("moveKeyCode", String.valueOf(keyCode));
+        return new BaseElement("move").addAttribute("moveKeyCode", String.valueOf(keyCode)).addAttribute("app",
+                String.valueOf(app));
     }
 
     /**
@@ -22,13 +37,18 @@ public record directionMove(App app, int keyCode) implements Move{
      */
     @Override
     public void move() {
-       Controller c = app.getGame().phase().controller();
-       c.keyPressed(keyCode);
+        Controller c = app.getGame().phase().controller();
+        c.keyPressed(keyCode);
     }
 
     @Override
     public void undo() {
         // not yet handled
-        
+
     }
+
+    public App getApp() {
+        return this.app;
+    }
+
 }
