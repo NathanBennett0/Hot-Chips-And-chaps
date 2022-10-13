51ff34243160a018b25829be458721cf63142c37
Christine Jayme <jaymechri@ecs.vuw.ac.nz>
2022-10-13 21:34:15 +1300

    close #51 Creating JavaDocs for fields

--- src/nz/ac/vuw/ecs/swen225/gp22/Recorder/directionMove.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/Recorder/directionMove.java
@@ -5,9 +5,23 @@ import nz.ac.vuw.ecs.swen225.gp22.app.App;
 import nz.ac.vuw.ecs.swen225.gp22.app.Controller;
 
 public class directionMove implements Move {
+    /**
+     * keyCode for the move.
+     * keyCode is required in order to use the keyPressed method inside
+     * KeyStroke.java
+     */
     public int keyCode = 0;
+
+    /**
+     * Gets the app from app class
+     */
     App app = App.getInstance();
 
+    /**
+     * Constructor for the Move
+     * 
+     * @param code keyCode for the directionMove
+     */
     public directionMove(int code) {
         keyCode = code;
     }
@@ -20,7 +34,7 @@ public class directionMove implements Move {
         Controller c = app.getPhase().controller();
         c.keyPressed(keyCode);
     }
-    
+
     /**
      * Return the key code for this directionMove
      * 
@@ -32,6 +46,7 @@ public class directionMove implements Move {
 
     /**
      * Returns app for this directionMove
+     * 
      * @return app
      */
     public App getApp() {
