5cb9bd0fd9959f2d5bc206c20a9ce0bf43f7e547
Christine Jayme <jaymechri@ecs.vuw.ac.nz>
2022-10-13 19:31:38 +1300

    Cleaning up code and comments

--- src/nz/ac/vuw/ecs/swen225/gp22/Recorder/Move.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/Recorder/Move.java
@@ -6,21 +6,13 @@ import org.dom4j.Element;
  * Interface to be implemented for each Move
  */
 public interface Move {
-    /**
-      * Save the move in an XML format
-      
-    Element saveXML();
-
-    */
-      
-    /**
+  /**
      * Performs the move.
      */
     void move();
-    
+  
     /**
      * Undoes the action.
+     * void undo();
      */
-
-     void undo();
 }
